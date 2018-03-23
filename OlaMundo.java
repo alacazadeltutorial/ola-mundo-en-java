a14f4f3cc0dfc14e2ce005cf1d67571ffc6692d7
Alberto Solano <alacazadeltutorial@gmail.com>
2018-03-23 18:56:11 +0100

    Comentarios engadidos ao código

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,3 +1,8 @@
+/**  
+ * Mostra por pantalla a frase de sempre 
+ * @author alacazadeltutorial
+*/
+
 public class OlaMundo {
 	public static void main (String[] args) {
 		System.out.println("Olá mundo!");
