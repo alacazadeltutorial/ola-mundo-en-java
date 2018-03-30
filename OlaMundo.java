dad1fe9e7a9b712f57f1c14e5afa10b03f5cf540
Alberto Solano <alacazadeltutorial@gmail.com>
2018-03-30 17:36:46 +0200

    Cores definidas

--- OlaMundo.java
+++ OlaMundo.java
@@ -5,6 +5,8 @@
 
 public class OlaMundo {
 	public static void main (String[] args) {
+		String laranxa = "\033[33m";
+		String azul = "\033[34m";
 		System.out.println("Olá mundo!");
 		System.out.println("Aqui estamos a facer probas co Git");
 	}
